693cd1e9efa2936ba6c17488412f3e68533d3891
Barney <barney.carroll@gmail.com>
2014-11-12 14:37:29 +0000

    Removed unwanted deps, added my alternatives * Removed:     * Coffeescript     * Handlebarsify     * jQuery     * Backbone     * config-based Browserify execution * Added:     * es6ify     * Mithril     * Ramda     * msx

--- gulp/config.js
+++ gulp/config.js
@@ -29,16 +29,14 @@ module.exports = {
   browserify: {
     // Enable source maps
     debug: true,
-    // Additional file extentions to make optional
-    extensions: ['.coffee', '.hbs'],
     // A separate bundle will be generated for each
     // bundle config in the list below
     bundleConfigs: [{
-      entries: src + '/javascript/app.coffee',
+      entries: src + '/javascript/app.js',
       dest: dest,
       outputName: 'app.js'
     }, {
-      entries: src + '/javascript/head.coffee',
+      entries: src + '/javascript/head.js',
       dest: dest,
       outputName: 'head.js'
     }]
